de3645bf7aad354c82d9aa9c4c86812b3a34068d
Samuel G <85844486+Sxmuray@users.noreply.github.com>
2022-04-21 00:20:33 -0300

    Create JoinGroups.java

---
+++ org/github/sxmuray/JoinGroups.java
@@ -0,0 +1,10 @@
+package org.github.sxmuray;
+
+public class JoinGroups extends Exception
+{
+	public StartupException(String message) { super(message); }
+
+	public StartupException(Throwable cause) { super(cause); }
+
+	public StartupException(String message, Throwable cause) { super(message, cause); }
+}
